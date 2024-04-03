13ef534700ca672b7f4c88fd94d4cbb663e4c577
Tarick <gth766q@gatech.edu>
2024-04-02 20:29:33 -0400

    configure tailwind & concurrently

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const { blackA, violet, mauve } = require("@radix-ui/colors");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./App.jsx"],
+  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
