395cf85c5dbec83ea04168734c7a37aaa4aa99c7
Tarick <gth766q@gatech.edu>
2024-04-04 17:34:04 -0400

    modular styling; moar pixel perfection

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,9 @@ function App() {
     <div className="App">
       <div className="container w-[350px] lg:w-[650px]">
         <h1 className="text-3xl font-semibold">One-time Loan Payment</h1>
-        <h3>Fill out the form below to complete your payment.</h3>
+        <h3 className="mb-[30px]">
+          Fill out the form below to complete your payment.
+        </h3>
         <Form />
       </div>
     </div>
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -22,78 +22,69 @@ const FormDemo = () => {
   return (
     <Form.Root className="grid grid-cols-1 lg:grid-cols-3">
       <Form.Field
-        className="col-span-1 lg:col-span-3 border-t border-r border-l p-[5px]"
+        className="Form-Field col-span-1 lg:col-span-3 border-t border-r border-l"
         name="loan-account-number"
       >
         <div className="flex items-baseline justify-between">
-          <Form.Label className="text-[12px] font-semibold leading-none text-black">
+          <Form.Label className="Form-Field__Form-Label">
             Loan Account Number
           </Form.Label>
-          <Form.Message
-            className="text-[13px] text-black opacity-[0.8]"
-            match="valueMissing"
-          >
-            Loan Account Number is required
-          </Form.Message>
-          <Form.Message
-            className="text-[13px] text-black opacity-[0.8]"
-            match={(value) => !validateAccountNumber(value)}
-          >
-            Please provide a valid Loan Account Number
-          </Form.Message>
         </div>
         <Form.Control asChild>
           <input
-            className="box-border w-full inline-flex h-[35px] appearance-none items-center justify-center px-[10px] text-[16px] leading-none text-black outline-none hover:bg-blackA2 focus:bg-blackA2"
+            className="Form-Field__Form-Control__input"
             type="text"
             required
           />
         </Form.Control>
+        <Form.Message className="Form-Field__Form-Message" match="valueMissing">
+          Loan Account Number is required
+        </Form.Message>
+        <Form.Message
+          className="Form-Field__Form-Message"
+          match={(value) => !validateAccountNumber(value)}
+        >
+          Please provide a valid Loan Account Number
+        </Form.Message>
       </Form.Field>
       <Form.Field
-        className="col-span-1 w-[360px] lg:col-span-2 border-t border-r border-l p-[5px]"
+        className="Form-Field col-span-1 w-[360px] lg:col-span-2 border-t border-r border-l"
         name="account-type"
       >
         <div className="flex items-baseline justify-between">
-          <Form.Label className="text-[12px] font-semibold leading-none text-black">
+          <Form.Label className="Form-Field__Form-Label">
             Type of Account:
           </Form.Label>
         </div>
         <Form.Control asChild>
           <RadioGroup.Root
-            className="flex flex-row gap-[35px]"
+            className="flex flex-row items-center gap-[35px] h-[100%] mt-[-15px] ml-[5px]"
             defaultValue="checking"
             aria-label="View density"
             required
             ref={radioRef}
           >
-            <div className="flex items-center">
+            <div className="flex">
               <RadioGroup.Item
-                className="aria-checked:bg-blue-500 bg-white w-[15px] h-[15px] rounded-full shadow-[0_0_2px] shadow-grayA4 hover:bg-blue-100 focus:bg-blue-500 outline-none cursor-default"
+                className="RadioGroup-Item"
                 value="checking"
                 id="r1"
               >
-                <RadioGroup.Indicator className="flex items-center justify-center w-full h-full relative after:content-[''] after:block after:w-[5px] after:h-[5px] after:rounded-[50%] after:bg-white" />
+                <RadioGroup.Indicator className="RadioGroup-Indicator" />
               </RadioGroup.Item>
-              <label
-                className="text-black text-[14px] leading-none pl-[5px]"
-                htmlFor="r1"
-              >
+              <label className="RadioGroup-Root__label" htmlFor="r1">
                 Checking
               </label>
             </div>
-            <div className="flex items-center">
+            <div className="flex">
               <RadioGroup.Item
-                className="aria-checked:bg-blue-500 bg-white w-[15px] h-[15px] rounded-full shadow-[0_0_2px] shadow-grayA4 hover:bg-blue-100 focus:bg-blue-500 outline-none cursor-default"
+                className="RadioGroup-Item"
                 value="debit"
                 id="r2"
               >
-                <RadioGroup.Indicator className="flex items-center justify-center w-full h-full relative after:content-[''] after:block after:w-[5px] after:h-[5px] after:rounded-[50%] after:bg-white" />
+                <RadioGroup.Indicator className="RadioGroup-Indicator" />
               </RadioGroup.Item>
-              <label
-                className="text-black text-[14px] leading-none pl-[5px]"
-                htmlFor="r2"
-              >
+              <label className="RadioGroup-Root__label" htmlFor="r2">
                 Debit Card
               </label>
             </div>
@@ -101,10 +92,10 @@ const FormDemo = () => {
         </Form.Control>
       </Form.Field>
       <div
-        className="w-[295px] col-span-1 lg:row-span-4 border-t border-r border-b p-[5px] bg-indigo-100 bg-no-repeat ml-[-78px]"
+        className="w-[291px] col-span-1 lg:row-span-4 border-t border-r border-b p-[5px] bg-indigo-100 bg-no-repeat ml-[-74px]"
         style={{
           backgroundImage: "url(/img/check.png)",
-          backgroundPositionX: "-13px",
+          backgroundPositionX: "-15px",
           backgroundPositionY: "56px",
         }}
       >
@@ -115,97 +106,82 @@ const FormDemo = () => {
         </h3>
       </div>
       <Form.Field
-        className="col-span-1 w-[360px] lg:col-span-2 border-t border-r border-l p-[5px]"
+        className="Form-Field col-span-1 w-[360px] lg:col-span-2 border-t border-r border-l"
         name="routing-number"
       >
         <div className="flex items-baseline justify-between">
-          <Form.Label className="text-[12px] font-semibold leading-none text-black">
+          <Form.Label className="Form-Field__Form-Label">
             Routing Number
           </Form.Label>
-          <Form.Message
-            className="text-[13px] text-black opacity-[0.8]"
-            match="valueMissing"
-          >
-            Routing Number is required
-          </Form.Message>
-          <Form.Message
-            className="text-[13px] text-black opacity-[0.8]"
-            match={(value) => !validateAccountNumber(value, ROUTING_LEN)}
-          >
-            Please provide a valid 9-digit Routing Number
-          </Form.Message>
         </div>
         <Form.Control asChild>
           <input
-            className="box-border w-full inline-flex h-[35px] appearance-none items-center justify-center px-[10px] text-[16px] leading-none text-black outline-none hover:bg-blackA2 focus:bg-blackA2"
+            className="Form-Field__Form-Control__input"
             type="text"
             required
           />
         </Form.Control>
+        <Form.Message className="Form-Field__Form-Message" match="valueMissing">
+          Routing Number is required
+        </Form.Message>
+        <Form.Message
+          className="Form-Field__Form-Message"
+          match={(value) => !validateAccountNumber(value, ROUTING_LEN)}
+        >
+          Please provide a valid 9-digit Routing Number
+        </Form.Message>
       </Form.Field>
       <Form.Field
-        className="col-span-1 w-[360px] lg:col-span-2 border-t border-r border-l p-[5px]"
+        className="Form-Field col-span-1 w-[360px] lg:col-span-2 border-t border-r border-l"
         name="bank-account-number"
       >
         <div className="flex items-baseline justify-between">
-          <Form.Label className="text-[12px] font-semibold leading-none text-black">
+          <Form.Label className="Form-Field__Form-Label">
             Bank Account Number
           </Form.Label>
-          <Form.Message
-            className="text-[13px] text-black opacity-[0.8]"
-            match="valueMissing"
-          >
-            Bank Account Number is required
-          </Form.Message>
-          <Form.Message
-            className="text-[13px] text-black opacity-[0.8]"
-            match="typeMismatch"
-          >
-            Please provide a valid Bank Account Number
-          </Form.Message>
         </div>
         <Form.Control asChild>
           <input
-            className="box-border w-full inline-flex h-[35px] appearance-none items-center justify-center px-[10px] text-[16px] leading-none text-black outline-none hover:bg-blackA2 focus:bg-blackA2"
+            className="Form-Field__Form-Control__input"
             type="text"
             required
           />
         </Form.Control>
+        <Form.Message className="Form-Field__Form-Message" match="valueMissing">
+          Bank Account Number is required
+        </Form.Message>
+        <Form.Message className="Form-Field__Form-Message" match="typeMismatch">
+          Please provide a valid Bank Account Number
+        </Form.Message>
       </Form.Field>
       <Form.Field
-        className="col-span-1  w-[360px] lg:col-span-2 border p-[5px]"
+        className="Form-Field col-span-1  w-[360px] lg:col-span-2 border"
         name="bank-account-number-confirm"
       >
         <div className="flex items-baseline justify-between">
-          <Form.Label className="text-[12px] font-semibold leading-none text-black">
+          <Form.Label className="Form-Field__Form-Label">
             Confirm Bank Account Number
           </Form.Label>
-          <Form.Message
-            className="text-[13px] text-black opacity-[0.8]"
-            match="valueMissing"
-          >
-            Bank Account Numbers must match
-          </Form.Message>
-          <Form.Message
-            className="text-[13px] text-black opacity-[0.8]"
-            match="typeMismatch"
-          >
-            Please provide a valid Bank Account Number
-          </Form.Message>
         </div>
         <Form.Control asChild>
           <input
-            className="box-border w-full inline-flex h-[35px] appearance-none items-center justify-center px-[10px] text-[16px] leading-none text-black outline-none hover:bg-blackA2 focus:bg-blackA2"
+            className="Form-Field__Form-Control__input"
             type="text"
             required
           />
         </Form.Control>
+        <Form.Message className="Form-Field__Form-Message" match="valueMissing">
+          Bank Account Numbers must match
+        </Form.Message>
+        <Form.Message className="Form-Field__Form-Message" match="typeMismatch">
+          Please provide a valid Bank Account Number
+        </Form.Message>
       </Form.Field>
       <Form.Submit
         asChild
         className="lg:col-span-3 md:col-span-1 sm:col-span-1"
       >
-        <button className="box-border w-full text-white shadow-blackA4 hover:bg-mauve3 inline-flex h-[50px] w-[200px] items-center justify-center bg-teal-500 px-[15px] font-semibold leading-none shadow-[0_2px_10px] focus:shadow-[0_0_0_2px] focus:shadow-black focus:outline-none mt-[10px]">
+        <button className="box-border w-[210px] text-white hover:bg-mauve3 inline-flex h-[50px] w-[200px] items-center justify-center bg-teal-500 px-[15px] font-semibold leading-none shadow-[0_2px_10px] focus:shadow-[0_0_0_2px] focus:shadow-black focus:outline-none mt-[20px]">
           {"Make Payment".toUpperCase()}
         </button>
       </Form.Submit>
